7b3a8411d599cf58be72d301e48eb76fc567a0e2
maurice <maurice.nzai@gmail.com>
2024-05-22 12:19:54 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 
+const errorHandler = require('./middlewares/error');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
@@ -10,9 +12,13 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
 
 app.get('/', (req, res) => {
  res.send('API is running...');
 });
+
+// Error Handling Middleware
+app.use(errorHandler);
 
 module.exports = app;
